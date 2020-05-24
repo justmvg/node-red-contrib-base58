a9694f5ac886171fee757c7d5af30869e896dba7
JustMVG <michael@x06.nl>
2020-05-24 22:33:33 +0200

    Only allow type string

--- nodes/decoder/decoder.js
+++ nodes/decoder/decoder.js
@@ -6,11 +6,15 @@ module.exports = function (RED) {
         let node = this
 
         node.on('input', (msg) => {
+            if(typeof msg.payload === 'String'){
             const bytes = bs58.decode(msg.payload)
             let result = bytes.toString('hex')
 
             msg.payload.decoded = result
             node.send(msg);
+            }else {
+                this.warn("Input is not of type String.");
+            }
         })
     }
     RED.nodes.registerType('Base58 to HEX', b582hex)
--- nodes/encoder/encoder.js
+++ nodes/encoder/encoder.js
@@ -6,11 +6,15 @@ module.exports = function (RED) {
         let node = this
 
         node.on('input', (msg) => {
+            if(typeof msg.payload === 'String'){
             const bytes = Buffer.from(msg.payload, 'hex')
             const result = bs58.encode(bytes)
-            
+
             msg.payload.encoded = result
             node.send(msg);
+            }else {
+                this.warn("Input is not of type String.");
+            }
         })
     }
     RED.nodes.registerType('HEX to Base58', hex2b58)
